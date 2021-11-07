8738f990eab7f6a41bc90531add0bf742043423a
FauzaniRh <fauzanirahman737@gmail.com>
2021-11-07 21:59:30 +0800

    First Commit : Praktik video 3

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Text(
+          'Flutter App',
+          style: TextStyle(
+              color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 30),
+        ),
+        actions: [Icon(Icons.account_circle), Icon(Icons.access_alarm)],
+        centerTitle: true,
+      ),
+      body: Container(
+        color: Colors.blue,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        padding: EdgeInsets.all(30),
+        child: Icon(Icons.home, 
+        color: Colors.red,
+        size: 100.78,
+        ),
+      ),
+    );
+  }
+}
